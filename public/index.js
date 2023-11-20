27bd3c5ae84a5a732e0ebff913ac8b2b03514089
João Eduardo <joaoeduardoj920@gmail.com>
2023-11-20 16:20:49 -0300

    feat: Adding HTML to Test API

---
+++ public/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+
+const button = document.querySelector('button');
+const total = document.querySelector('p');
+const input = document.querySelector('input');
+
+total.style.display = 'none';
+
+const get_total_mutiple = async (number) => {
+  const response = await fetch('/v1/mutiples', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({ number }),
+  });
+
+  const data = await response.json();
+
+  return data;
+};
+
+const handleClickButton = async () => {
+  const number = +input.value;
+
+  total.style.display = 'block';
+
+  total.innerHTML = 'carregando...';
+
+  const data = await get_total_mutiple(number);
+
+  if (data.error) {
+    total.style.display = 'none';
+    alert(data.error);
+    return;
+  }
+
+  total.innerHTML = data.total;
+};
+
+button.addEventListener('click', handleClickButton);
--- src/app/index.ts
+++ src/app/index.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import { config } from 'dotenv';
 import { router } from './router';
 import { globalsErrorMiddleware } from './middleware/globals-errors';
+import path from 'path';
 
 const app = express();
 config();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.use(`${process.env.VERSION}`, router);
 
