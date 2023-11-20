32683d857de7f1132642b8154a83c92668e973b2
João Eduardo <joaoeduardoj920@gmail.com>
2023-11-20 14:30:13 -0300

    feat(vitest): initial config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    bail: 1,
+    globals: true,
+    environment: "node",
+    coverage: {
+      provider: "v8",
+    },
+    setupFiles: ["tests/vitest.setup.ts"],
+  },
+});
