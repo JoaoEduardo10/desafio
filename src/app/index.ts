eed1f0f0a8bc64675e95c316786a4989b3f2a475
João Eduardo <joaoeduardoj920@gmail.com>
2023-11-20 17:36:29 -0300

    feat(swagger): Adding API Documentation

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,11 +4,15 @@ import { config } from 'dotenv';
 import { router } from './router';
 import { globalsErrorMiddleware } from './middleware/globals-errors';
 import path from 'path';
+import swagger from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 config();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+
+app.use('/v1/desafio/documentation', swagger.serve, swagger.setup(swaggerDocs));
 
 app.use(`${process.env.VERSION}`, router);
 
